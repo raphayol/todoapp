bea63d43551073ec01872525b153f73d84af21e5
Raphael Egeley <raphael.egeley@gmail.com>
2020-11-18 14:54:22 +0100

    Make user tests pass, checking email address presence and format

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,6 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+  validates :email, presence: true
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
