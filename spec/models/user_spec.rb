5ed89d98e001d37520fb88b47544d5d863e9e321
Raphael Egeley <raphael.egeley@gmail.com>
2020-11-18 14:49:49 +0100

    User email address unit testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :uuid             not null, primary key
+#  email      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(email: 'test@test.com')
+  }
+
+  it "Should be valid with a valid email" do
+    expect(subject).to be_valid
+  end
+
+  it "Should not be valid with a blank email" do
+    subject.email = ""
+    expect(subject).to be_invalid
+  end
+
+  it "Should not be valid with a bad format email address" do
+    subject.email = "invalidformat"
+    expect(subject).to be_invalid
+  end
+
 end
